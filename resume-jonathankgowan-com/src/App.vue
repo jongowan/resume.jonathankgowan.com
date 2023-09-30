d446b53ae7be3774bcf3530fb0aa29eb8202f7c7
jongowan <jonathankgowan@gmail.com>
2023-09-30 11:25:28 -0500

    created main

--- resume-jonathankgowan-com/src/App.vue
+++ resume-jonathankgowan-com/src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
+import Main from './components/Main.vue'
 </script>
 
 <template>
-  <header>
+  <Main></Main>
+  <!-- <header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
@@ -14,7 +16,7 @@ import TheWelcome from './components/TheWelcome.vue'
 
   <main>
     <TheWelcome />
-  </main>
+  </main> -->
 </template>
 
 <style scoped>
